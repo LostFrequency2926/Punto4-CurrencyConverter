43c3caf3259a984bdd5d812d9e8cba977e504fa8
DANIEL MUNOZ <dafemure.180@gmail.com>
2023-03-11 16:46:12 -0500

    Se genera la migracion de la logica al VieModel en este caso se controla la entrada de parametros mediante el inputType, por lo tanto solo se genera validacion de que el campo no este vacio, en caso de que lo este limpia la vista resultado y muestra una alerta en caso contrario realizara la conversion

--- app/src/main/java/com/danielfmunoz/currencyconverter/ui/main/MainActivity.kt
+++ app/src/main/java/com/danielfmunoz/currencyconverter/ui/main/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.danielfmunoz.currencyconverter.ui.main
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.danielfmunoz.currencyconverter.R
 import com.danielfmunoz.currencyconverter.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
     private val currencies = listOf("COP", "EUR", "USD", "CAD", "CNY", "RUB")
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        val resultConvertObserver = Observer<Double> {resultConvert ->
+            mainBinding.textViewResultado.text = resultConvert.toString()
+        }
+
+        mainViewModel.resultConvert.observe(this, resultConvertObserver)
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencies)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -22,50 +36,17 @@ class MainActivity : AppCompatActivity() {
         mainBinding.spinnerDestino.adapter = adapter
 
         mainBinding.btnConvertir.setOnClickListener {
-            val cantidadStr = mainBinding.editTextCantidad.text.toString()
-            if (cantidadStr.isBlank()) {
+
+            if (mainViewModel.realizarValidateNulls(mainBinding.editTextCantidad.text.toString())) {
+                mainViewModel.convertCurrency(
+                    mainBinding.editTextCantidad.text.toString(),
+                    currencies[mainBinding.spinnerOrigen.selectedItemPosition],
+                    currencies[mainBinding.spinnerDestino.selectedItemPosition]
+                )
+            }else{
+                Toast.makeText(this, getString(R.string.wrongValue), Toast.LENGTH_SHORT).show()
                 mainBinding.textViewResultado.text = ""
-                mainBinding.editTextCantidad.error = "Ingresa una cantidad válida"
-                return@setOnClickListener
             }
-
-            val cantidad = cantidadStr.toDouble()
-            val origen = currencies[mainBinding.spinnerOrigen.selectedItemPosition]
-            val destino = currencies[mainBinding.spinnerDestino.selectedItemPosition]
-
-            val result = CurrencyConverter.convert(cantidad, origen, destino)
-
-            mainBinding.textViewResultado.text = "$cantidad $origen = $result $destino"
-            mainBinding.editTextCantidad.error = null
-        }
-    }
-
-    private object CurrencyConverter {
-        private val rates = mapOf(
-            "COP" to mapOf(
-                "COP" to 1.0, "EUR" to 0.00019, "USD" to 0.00021, "CAD" to 0.00028, "CNY" to 0.0014, "RUB" to 0.015
-            ),
-            "EUR" to mapOf(
-                "COP" to 5173.43, "EUR" to 1.0, "USD" to 1.07, "CAD" to 1.45, "CNY" to 7.33, "RUB" to 88.30
-            ),
-            "USD" to mapOf(
-                "COP" to 4848.28, "EUR" to 0.94, "USD" to 1.0, "CAD" to 1.36, "CNY" to 6.87, "RUB" to 75.27
-            ),
-            "CAD" to mapOf(
-                "COP" to 3567.36, "EUR" to 0.69, "USD" to 0.74, "CAD" to 1.0, "CNY" to 5.05, "RUB" to 55.38
-            ),
-            "CNY" to mapOf(
-                "COP" to 705.81, "EUR" to 0.14, "USD" to 0.15, "CAD" to 0.20, "CNY" to 1.0, "RUB" to 10.96
-            ),
-            "RUB" to mapOf(
-                "COP" to 64.42, "EUR" to 0.012, "USD" to 0.013, "CAD" to 0.018, "CNY" to 0.091, "RUB" to 1.0
-            )
-        )
-
-        fun convert(cantidad: Double, origen: String, destino: String): Double {
-            val rate = rates[origen]?.get(destino) ?: throw IllegalArgumentException("Tasas de cambio no encontradas para las monedas especificadas")
-            return cantidad * rate
         }
     }
 }
-
--- app/src/main/java/com/danielfmunoz/currencyconverter/ui/main/MainViewModel.kt
+++ app/src/main/java/com/danielfmunoz/currencyconverter/ui/main/MainViewModel.kt
@@ -1,7 +1,84 @@
 package com.danielfmunoz.currencyconverter.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
+    val resultConvert: MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+
+    private val currencies = listOf("COP", "EUR", "USD", "CAD", "CNY", "RUB")
+
+    fun convertCurrency(cantidadStr: String, origen: String, destino: String) {
+            val cantidad = cantidadStr.toDouble()
+            val result = CurrencyConverter.convert(cantidad, origen, destino)
+            resultConvert.value = result
+    }
+
+    fun realizarValidateNulls(cantidadStr: String): Boolean {
+        return !(cantidadStr.isEmpty())
+    }
+
+
+    private object CurrencyConverter {
+        private val rates = mapOf(
+            "COP" to mapOf(
+                "COP" to 1.0,
+                "EUR" to 0.00019,
+                "USD" to 0.00021,
+                "CAD" to 0.00028,
+                "CNY" to 0.0014,
+                "RUB" to 0.015
+            ),
+            "EUR" to mapOf(
+                "COP" to 5173.43,
+                "EUR" to 1.0,
+                "USD" to 1.07,
+                "CAD" to 1.45,
+                "CNY" to 7.33,
+                "RUB" to 88.30
+            ),
+            "USD" to mapOf(
+                "COP" to 4848.28,
+                "EUR" to 0.94,
+                "USD" to 1.0,
+                "CAD" to 1.36,
+                "CNY" to 6.87,
+                "RUB" to 75.27
+            ),
+            "CAD" to mapOf(
+                "COP" to 3567.36,
+                "EUR" to 0.69,
+                "USD" to 0.74,
+                "CAD" to 1.0,
+                "CNY" to 5.05,
+                "RUB" to 55.38
+            ),
+            "CNY" to mapOf(
+                "COP" to 705.81,
+                "EUR" to 0.14,
+                "USD" to 0.15,
+                "CAD" to 0.20,
+                "CNY" to 1.0,
+                "RUB" to 10.96
+            ),
+            "RUB" to mapOf(
+                "COP" to 64.42,
+                "EUR" to 0.012,
+                "USD" to 0.013,
+                "CAD" to 0.018,
+                "CNY" to 0.091,
+                "RUB" to 1.0
+            )
+        )
+
+        fun convert(cantidad: Double, origen: String, destino: String): Double {
+            val rate = rates[origen]?.get(destino)
+                ?: throw IllegalArgumentException("Tasas de cambio no encontradas para las monedas especificadas")
+            return cantidad * rate
+        }
+    }
 }
